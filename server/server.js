d545392a49e4815c7b6b50ec0907b58cadb2694c
Ryan Hood <ryanhood@ryans-mbp.lan>
2024-07-01 21:34:28 -0500

    switching to node fetch 2

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const path = require('path');
 const axios = require('axios');
 const { GoogleGenerativeAI } = require('@google/generative-ai');
+const fetch = require('node-fetch');  // Import node-fetch version 2
 
 const gemini_api_key = process.env.GEMINI_API_KEY;
 if (!gemini_api_key) {
@@ -85,9 +86,6 @@ app.post('/geminiCompletion', async (req, res) => {
 
     console.log('Gemini API Key:', gemini_api_key);
     console.log('Gemini Config:', geminiConfig);
-
-    // Import node-fetch dynamically
-    const fetch = (await import('node-fetch')).default;
 
     const geminiModel = googleAI.getGenerativeModel({
       model: 'gemini-pro',
